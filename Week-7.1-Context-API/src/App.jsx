36f667492dda3b0d56af4eb76c4af60e4bda6a61
Suryansh <suryanshrao01@gmail.com>
2024-07-09 14:53:48 +0530

    Client-Side-Routing

--- Week-7.1-Context-API/src/App.jsx
+++ Week-7.1-Context-API/src/App.jsx
@@ -1,13 +1,30 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import { Dashboard } from "./components/Dashboard";
+import { Landing } from "./components/Landing";
 
 function App() {
   return (
+    <div>
+      <div>
+        <button onClick={function(){
+          window.location.href = "/";
+        }}>Landing Page</button>
+        <br />
+        <br />
+        <button onClick={function(){
+          window.location.href = "/dashboard"
+        }}>Dashboard</button>
+        <br />
+        <br />
+      </div>
     <BrowserRouter>
       <Routes>
-        
+        <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
+        <Route path="/" element={<Landing/>}></Route>
       </Routes>
     </BrowserRouter>
+    </div>
   );
 }
 
