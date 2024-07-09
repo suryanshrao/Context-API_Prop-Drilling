cc06228576bbf95e092b45130635162cd7c49697
Suryansh <suryanshrao01@gmail.com>
2024-07-09 14:37:46 +0530

    Made Component And Routes

--- Week-7.1-Context-API/src/App.jsx
+++ Week-7.1-Context-API/src/App.jsx
@@ -1,17 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes } from "react-router-dom";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
   return (
-    <>
-      <button onClick={function(){
-        setCount(count + 1)
-      }}>Counter {count}</button>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
